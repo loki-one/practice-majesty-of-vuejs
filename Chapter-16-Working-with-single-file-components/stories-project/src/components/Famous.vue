8ffdf74951321a7c573d120e71647f909e989b56
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2018-02-17 11:55:54 +0100

    :hammer: Separate stories in new store.js to avoid repeating it again and again

--- Chapter-16-Working-with-single-file-components/stories-project/src/components/Famous.vue
+++ Chapter-16-Working-with-single-file-components/stories-project/src/components/Famous.vue
@@ -7,6 +7,8 @@
   </ul>
 </template>
 <script>
+import {store} from '../store.js'
+
 export default {
   computed: {
     famous: function () {
@@ -18,32 +20,7 @@ export default {
   },
   data: function () {
     return {
-      stories: [
-        {
-          plot: 'My horse is amazing.',
-          writer: 'Mr. Weebl',
-          upvotes: 28,
-          voted: false
-        },
-        {
-          plot: 'Narwhals invented Shish Kebab.',
-          writer: 'Mr. Weebl',
-          upvotes: 8,
-          voted: false
-        },
-        {
-          plot: 'The dark side of the Force is stronger.',
-          writer: 'Darth Vader',
-          upvotes: 52,
-          voted: false
-        },
-        {
-          plot: 'One does not simply walk into Mordor',
-          writer: 'Boromir',
-          upvotes: 74,
-          voted: false
-        }
-      ]
+      stories: store.stories
     }
   }
 }
--- Chapter-16-Working-with-single-file-components/stories-project/src/components/Stories.vue
+++ Chapter-16-Working-with-single-file-components/stories-project/src/components/Stories.vue
@@ -8,35 +8,12 @@
 </template>
 
 <script>
+import {store} from '../store.js'
+
 export default {
   data: function () {
     return {
-      stories: [
-        {
-          plot: 'My horse is amazing.',
-          writer: 'Mr. Weebl',
-          upvotes: 28,
-          voted: false
-        },
-        {
-          plot: 'Narwhals invented Shish Kebab.',
-          writer: 'Mr. Weebl',
-          upvotes: 8,
-          voted: false
-        },
-        {
-          plot: 'The dark side of the Force is stronger.',
-          writer: 'Darth Vader',
-          upvotes: 52,
-          voted: false
-        },
-        {
-          plot: 'One does not simply walk into Mordor',
-          writer: 'Boromir',
-          upvotes: 74,
-          voted: false
-        }
-      ]
+      stories: store.stories
     }
   }
 }
