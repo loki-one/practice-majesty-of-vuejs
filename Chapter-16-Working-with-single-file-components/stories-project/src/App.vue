fa28a4dfddbfc1a7ca14ce12b4b7bd19c4321bab
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2018-02-17 18:55:49 +0100

    :fire: simple navigation system by swapping components

--- Chapter-16-Working-with-single-file-components/stories-project/src/App.vue
+++ Chapter-16-Working-with-single-file-components/stories-project/src/App.vue
@@ -1,38 +1,63 @@
 <template>
   <div id="app" class="container">
     <img src="./assets/logo.png">
-    <component :is="currentComponent"></component>
+    <!-- <component :is="currentComponent"></component>
     <p>
       This is very useful
     </p>
     <a href="#" @click="currentComponent = 'HelloWorld'">Show Hello</a>
-    <a href="#" @click="currentComponent = 'greet'">Show Greet</a>
+    <a href="#" @click="currentComponent = 'greet'">Show Greet</a> -->
     <!-- <HelloWorld/> -->
     <!-- <login></login> -->
     <!-- <register></register> -->
     <!-- <stories></stories> -->
+    <h1>Welcome to dynamic Components!</h1>
+    <ul class="nav nav-tabs">
+      <li :class="isActivePage(page) ? 'active' : ''" v-for="(page, index) in pages" :key="index">
+        <a @click="setPage(page)"> {{ page | capitalize }}</a>
+      </li>
+    </ul>
+    <component :is="activePage"></component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-import Greet from './components/Greet'
-// import Login from './components/Login'
-// import Register from './components/Register'
-// import Stories from './components/Stories'
+// import HelloWorld from './components/HelloWorld'
+// import Greet from './components/Greet'
+import Vue from 'vue'
+import Login from './components/Login'
+import Register from './components/Register'
+import Stories from './components/Stories'
+
+Vue.filter('capitalize', function (value) {
+  return value.charAt(0).toUpperCase() + value.substr(1)
+})
 
 export default {
   name: 'App',
   components: {
-    // Login,
-    // Register
-    // Stories
-    HelloWorld,
-    Greet
+    Login,
+    Register,
+    Stories
+    // HelloWorld,
+    // Greet
   },
   data: function () {
     return {
-      currentComponent: 'HelloWorld'
+      pages: [
+        'stories',
+        'register',
+        'login'
+      ],
+      activePage: 'stories'
+    }
+  },
+  methods: {
+    setPage: function (newPage) {
+      this.activePage = newPage
+    },
+    isActivePage: function (page) {
+      return this.activePage === page
     }
   }
 }
