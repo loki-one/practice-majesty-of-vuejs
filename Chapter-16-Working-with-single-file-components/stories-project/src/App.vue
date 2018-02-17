4038793ab519babf3107da133c5c45363d25061a
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2018-02-17 15:24:51 +0100

    :zap: Dynamic switching between the components HelloWorld and Greet using component and is attribute

--- Chapter-16-Working-with-single-file-components/stories-project/src/App.vue
+++ Chapter-16-Working-with-single-file-components/stories-project/src/App.vue
@@ -1,26 +1,39 @@
 <template>
   <div id="app" class="container">
     <img src="./assets/logo.png">
+    <component :is="currentComponent"></component>
+    <p>
+      This is very useful
+    </p>
+    <a href="#" @click="currentComponent = 'HelloWorld'">Show Hello</a>
+    <a href="#" @click="currentComponent = 'greet'">Show Greet</a>
     <!-- <HelloWorld/> -->
     <!-- <login></login> -->
-    <register></register>
+    <!-- <register></register> -->
     <!-- <stories></stories> -->
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld'
+import Greet from './components/Greet'
 // import Login from './components/Login'
-import Register from './components/Register'
+// import Register from './components/Register'
 // import Stories from './components/Stories'
 
 export default {
   name: 'App',
   components: {
     // Login,
-    Register
+    // Register
     // Stories
-    // HelloWorld
+    HelloWorld,
+    Greet
+  },
+  data: function () {
+    return {
+      currentComponent: 'HelloWorld'
+    }
   }
 }
 </script>
